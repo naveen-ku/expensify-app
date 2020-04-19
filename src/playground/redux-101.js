e33264bff0a9ef5331b8714667dea44c20bf817f
naveen-ku <naveen2951996@gmail.com>
2020-04-19 17:16:43 +0530

    Dispatching the actions in redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,54 @@
 import { createStore } from "redux";
+console.log("Playground Redux");
+const store = createStore((state = { count: 0 }, action) => {
+  console.log("running store");
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1,
+      };
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+    case "DECREMENT":
+      return {
+        count: state.count - 1,
+      };
+    case "RESET":
+      return {
+        count: (state.count = 0),
+      };
+
+    default:
+      return state;
+  }
 });
+console.log(store.getState());
 
-console.log(store.getState())
+store.dispatch({
+  type: "INCREMENT",
+});
+console.log(store.getState());
+
+store.dispatch({
+  type: "DECREMENT",
+});
+console.log(store.getState());
+
+store.dispatch({
+  type: "DECREMENT",
+});
+console.log(store.getState());
+
+store.dispatch({
+  type: "DECREMENT",
+});
+console.log(store.getState());
+
+store.dispatch({
+  type: "DECREMENT",
+});
+console.log(store.getState());
+
+store.dispatch({
+  type: "RESET",
+});
+console.log(store.getState());
