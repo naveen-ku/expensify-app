9ba076232916a9b310e681cd65dc08be030a4a65
naveen-ku <naveen2951996@gmail.com>
2020-04-20 13:37:00 +0530

    Dynamic use of action in redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,54 +1,59 @@
 import { createStore } from "redux";
 console.log("Playground Redux");
 const store = createStore((state = { count: 0 }, action) => {
-  console.log("running store");
+  // console.log("running store");
+
   switch (action.type) {
     case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1,
+        count: state.count + incrementBy,
       };
 
     case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1,
+        count: state.count - decrementBy,
       };
     case "RESET":
       return {
         count: (state.count = 0),
+      };
+    case "SET":
+      return {
+        count: action.count,
       };
 
     default:
       return state;
   }
 });
-console.log(store.getState());
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 store.dispatch({
   type: "INCREMENT",
+  incrementBy: 5,
 });
-console.log(store.getState());
+
+store.dispatch({
+  type: "DECREMENT",
+  decrementBy: 20,
+});
 
 store.dispatch({
   type: "DECREMENT",
 });
-console.log(store.getState());
-
-store.dispatch({
-  type: "DECREMENT",
-});
-console.log(store.getState());
-
-store.dispatch({
-  type: "DECREMENT",
-});
-console.log(store.getState());
-
-store.dispatch({
-  type: "DECREMENT",
-});
-console.log(store.getState());
 
 store.dispatch({
   type: "RESET",
 });
-console.log(store.getState());
+
+store.dispatch({
+  type: "SET",
+  count: 111,
+});
