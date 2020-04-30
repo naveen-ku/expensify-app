0d003424ea6dca658097ca98e8102661dc881fe3
naveen-ku <naveen2951996@gmail.com>
2020-04-30 16:47:23 +0530

    Organized redux file into small files

--- src/app.js
+++ src/app.js
@@ -3,5 +3,27 @@ import ReactDOM from "react-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+
+const store = configureStore();
+
+//addExpense ->Water bill
+store.dispatch(addExpense({ description: "Water Bill", amount: 50 }));
+console.log(store.getState());
+//addExpense ->Gas bill
+store.dispatch(addExpense({ description: "Gas Bill", amount: 1000 }));
+console.log(store.getState());
+//setTextFilter
+store.dispatch(setTextFilter("water"));
+console.log(store.getState());
+
+//getVisibleExpenses
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log("visibleExpenses: ", visibleExpenses);
+
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
