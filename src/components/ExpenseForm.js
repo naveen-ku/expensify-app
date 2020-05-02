efae63ed10dff85408b3beddb90a5b4091688611
naveen-ku <naveen2951996@gmail.com>
2020-05-02 16:14:20 +0530

    Setting up a date picker

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,4 +1,10 @@
 import React from "react";
+import moment from "moment";
+import { SingleDatePicker } from "react-dates";
+import "../../node_modules/react-dates/lib/css/_datepicker.css";
+
+const now = moment();
+setTimeout(console.log(now.format("MMM do YYYY")), 2000);
 
 class ExpenseForm extends React.Component {
   constructor(props) {
@@ -7,10 +13,14 @@ class ExpenseForm extends React.Component {
       description: "",
       note: "",
       amount: "",
+      createdAt: moment(),
+      calenderFocused: false,
     };
     this.onDescriptionChange = this.onDescriptionChange.bind(this);
     this.onNoteChange = this.onNoteChange.bind(this);
     this.onAmountChange = this.onAmountChange.bind(this);
+    this.onDateChange = this.onDateChange.bind(this);
+    this.onFocusChange = this.onFocusChange.bind(this);
   }
 
   onDescriptionChange(e) {
@@ -21,7 +31,12 @@ class ExpenseForm extends React.Component {
     const note = e.target.value;
     this.setState(() => ({ note }));
   }
-
+  onDateChange(createdAt) {
+    this.setState(() => ({ createdAt }));
+  }
+  onFocusChange({ focused }) {
+    this.setState(() => ({ calenderFocused: focused }));
+  }
   onAmountChange(e) {
     const amount = e.target.value;
     if (amount.match(/^\d*(\.\d{0,2})?$/)) {
@@ -45,6 +60,14 @@ class ExpenseForm extends React.Component {
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calenderFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
           />
           <textarea
             placeholder="Add a note for expense(optional)"
