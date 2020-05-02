0051bbcc2c489a0c696d1c76c53ac8676763b982
naveen-ku <naveen2951996@gmail.com>
2020-05-02 16:56:26 +0530

    Completing functionality of add expense page

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -15,12 +15,33 @@ class ExpenseForm extends React.Component {
       amount: "",
       createdAt: moment(),
       calenderFocused: false,
+      error: "",
     };
     this.onDescriptionChange = this.onDescriptionChange.bind(this);
     this.onNoteChange = this.onNoteChange.bind(this);
     this.onAmountChange = this.onAmountChange.bind(this);
     this.onDateChange = this.onDateChange.bind(this);
     this.onFocusChange = this.onFocusChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault(); //prevent full relload of pages
+    if (!this.state.description || !this.state.amount) {
+      //Set error state equal to "Please provide description and amount"
+      this.setState(() => ({ error: "Please provide description and amount" }));
+    } else {
+      //Clear the error
+      this.setState(() => {
+        error: "";
+      });
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10),
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state,
+      });
+    }
   }
 
   onDescriptionChange(e) {
@@ -32,14 +53,16 @@ class ExpenseForm extends React.Component {
     this.setState(() => ({ note }));
   }
   onDateChange(createdAt) {
-    this.setState(() => ({ createdAt }));
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    }
   }
   onFocusChange({ focused }) {
     this.setState(() => ({ calenderFocused: focused }));
   }
   onAmountChange(e) {
     const amount = e.target.value;
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
   }
@@ -47,7 +70,8 @@ class ExpenseForm extends React.Component {
   render() {
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
