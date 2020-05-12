b0270c5df8976bb37c0aa15afa4273cf4f5cf8bf
naveen-ku <naveen2951996@gmail.com>
2020-05-12 09:40:59 +0530

    Writing test cases for expenses Selector file

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -19,9 +19,9 @@ export default (expenses, { text, sortBy, startDate, endDate }) => {
     })
     .sort((a, b) => {
       if (sortBy === "date") {
-        return a.createdAt < b.createdAt ? 1 : -1;
+        return b.createdAt - a.createdAt;
       } else if (sortBy === "amount") {
-        return a.amount < b.amount ? 1 : -1;
+        return b.amount - a.amount;
       }
     });
 };
