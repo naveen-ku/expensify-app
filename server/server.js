0bba8ce217fedc10c96881365abcc22f73004954
naveen-ku <naveen2951996@gmail.com>
2020-05-25 17:01:15 +0530

    Setup devDependencies, dist folder and removing some errors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
+const publicPath = path.join(__dirname, "/..", "/public");
 
 app.use(express.static(publicPath));
 
