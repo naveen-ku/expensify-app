f29e824986966cb9e51123b9d626f5d967ea23e0
naveen-ku <naveen2951996@gmail.com>
2020-05-25 13:09:39 +0530

    Removing dummy data

--- src/app.js
+++ src/app.js
@@ -13,41 +13,6 @@ import "../node_modules/react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-//addExpense ->Water bill
-store.dispatch(
-  addExpense({
-    description: "Water Bill",
-    amount: 1500,
-    createdAt: 1589024204966,
-  })
-);
-// console.log(store.getState());
-
-//addExpense ->Gas bill
-store.dispatch(
-  addExpense({ description: "Gas Bill", createdAt: 1589024204953 })
-);
-// console.log(store.getState());
-
-//addExpense ->Rent bill
-store.dispatch(
-  addExpense({ description: "Rent Bill", amount: 23, createdAt: 1589024204434 })
-);
-// console.log(store.getState());
-
-//setTextFilter
-// store.dispatch(setTextFilter("water"));
-// console.log(store.getState());
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter("bill"));
-// }, 3000);
-
-//getVisibleExpenses
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log("visibleExpenses: ", visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
