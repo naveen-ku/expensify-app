18232558436baf4c441907a34cfbe1a5bb4ab7a6
naveen-ku <naveen2951996@gmail.com>
2020-06-09 17:21:45 +0530

    Adding the login and logout functionality

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { startSetExpenses } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+import "react-dates/lib/css/_datepicker.css";
+import { firebase } from "./firebase/firebase";
 
 const store = configureStore();
 
@@ -19,8 +19,16 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById("root"));
 
 store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('root'));
+  ReactDOM.render(jsx, document.getElementById("root"));
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log("login");
+  } else {
+    console.log("logout");
+  }
 });
